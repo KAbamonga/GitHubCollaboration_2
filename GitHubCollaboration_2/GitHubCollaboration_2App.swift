b8b1e756ed20368e259fc9f21445f7c45b2ef381
Kiara Eunice Abamonga <kiara.abamonga@gmail.com>
2025-06-09 09:21:30 -0500

    Initial Commit

---
+++ GitHubCollaboration_2/GitHubCollaboration_2App.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubCollaboration_2App.swift
+//  GitHubCollaboration_2
+//
+//  Created by Kiara on 6/9/25.
+//
+
+import SwiftUI
+
+@main
+struct GitHubCollaboration_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
